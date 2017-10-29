6aaef8d243c5fdfe5de9fe4899b81eafd247b0f8
Tanjin Panna <tanjin.panna@gmail.com>
2017-10-28 21:53:15 -0400

    allowing run script to accept arguments

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-#python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
+
+echo "Let's find some political donor data!"
+inputFile="./input/itcont.txt"
+byZipOutputFile="./output/medianvals_by_zip.txt"
+byDateOutputFile="./output/medianvals_by_date.txt"
+
+sbt "run $inputFile $byZipOutputFile $byDateOutputFile"
+
+echo "Data processing is complete. Go check the output files!"
