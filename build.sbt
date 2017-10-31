27e62bd2d03e77fe7f648e34eafa694a08befd9c
Tanjin Panna <tanjin.panna@gmail.com>
2017-10-30 23:47:37 -0400

    converting from sbt project to simple scala script/file

--- build.sbt
+++
@@ -1,9 +0,0 @@
-name := "find political donors"
-
-version := "1.0"
-
-scalaVersion := "2.12.4"
-
-libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
-)
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ INPUT_FILE="./input/itcont.txt"
 ZIP_OUTPUT_FILE="./output/medianvals_by_zip.txt"
 DATE_OUTPUT_FILE="./output/medianvals_by_date.txt"
 
-scala src/main/scala/Main.scala $INPUT_FILE $ZIP_OUTPUT_FILE $DATE_OUTPUT_FILE
+scala src/Main.scala $INPUT_FILE $ZIP_OUTPUT_FILE $DATE_OUTPUT_FILE
